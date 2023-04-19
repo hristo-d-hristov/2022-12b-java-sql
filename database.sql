ba6b4200d6f5e253da93bd7d4e81495a0002cf11
Hristo Hristov <hristo.dr.hristov@gmail.com>
2023-04-19 10:54:44 +0300

    add database

--- database.sql
+++ database.sql
@@ -1,6 +1,24 @@
 DROP DATABASE IF EXISTS traktori;
 CREATE DATABASE traktori;
 USE traktori;
-CREATE TABLE marki (id INT, marka VARCHAR(255));
-CREATE TABLE potrebiteli (name VARCHAR(255), pass VARCHAR(255), mail VARCHAR(255));
-INSERT INTO marki VALUES (1, "Bulgar"), (2, "UMZ");
+
+CREATE TABLE potrebiteli (
+	name VARCHAR(255),
+	pass VARCHAR(255),
+	mail VARCHAR(255)
+);
+
+CREATE TABLE traktori (
+	id INT,
+	marka VARCHAR(255),
+	snimka VARCHAR(255),
+	cena INT
+);
+
+INSERT INTO potrebiteli VALUES
+	("admin", "admin", "admin@traktori.bg"),
+	("guest", "1234", "guest@traktori.bg");
+	
+INSERT INTO traktori VALUES
+	(1, "Bulgar", "bulgar.jpg", 5),
+	(2, "UMZ", "umz.jpg", 7);
--- src/java/LoginServlet.java
+++ src/java/LoginServlet.java
@@ -13,9 +13,19 @@ public class LoginServlet extends HttpServlet {
             throws ServletException, IOException {
         String user = request.getParameter("user");
         String pass = request.getParameter("pass");
+        String successful = """
+                            <!DOCTYPE html>
+                            <html>
+                                <head>
+                                </head>
+                                <body>
+                                    Login %d successful %d!!!
+                                </body>
+                            </html>
+                            """.formatted(5,7);
         if (user.equals("Hristov")
                 && pass.equals("1234")) {
-            response.getWriter().println("Login successful");
+            response.getWriter().println(successful);
         } else {
             response.getWriter().println("Login failed");
         }
