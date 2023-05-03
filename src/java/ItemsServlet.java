8f6da5386e7939c6cd86ba0ba318f24b521162a9
Hristo Hristov <hristo.dr.hristov@gmail.com>
2023-05-03 10:54:38 +0300

    cookies

--- src/java/ItemsServlet.java
+++ src/java/ItemsServlet.java
@@ -6,31 +6,30 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/LoginServlet"})
-public class LoginServlet extends HttpServlet {
+@WebServlet(urlPatterns = {"/ItemsServlet"})
+public class ItemsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response)
             throws ServletException, IOException {
-        String name = request.getParameter("name");
-        String pass = request.getParameter("pass");
-        String successful = """
+        String name = "";
+        Cookie[] cookies = request.getCookies();
+        for (Cookie c : cookies) {
+            if (c.getName().equals("name")) {
+                name = c.getValue();
+            }
+        }
+        
+        String webpage = """
             <!DOCTYPE html>
             <html>
                 <head>
                 </head>
                 <body>
-                    Login %d successful %d!!!
+                    Zdravei %s
                 </body>
             </html>
-            """.formatted(5,7);
-        if (name.equals("Hristov")
-                && pass.equals("1234")) {
-            Cookie c = new Cookie("name", name);
-            response.addCookie(c);
-            response.sendRedirect("index.html");
-        } else {
-            response.getWriter().println("Login failed");
-        }
+            """.formatted(name);
+        response.getWriter().println(webpage);
     }
 }
